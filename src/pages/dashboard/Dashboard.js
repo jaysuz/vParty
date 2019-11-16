60597a4529258af39664c8877a373d5d1b873152
Laquesisa <dana.shmaria@gmail.com>
2019-11-16 23:44:14 +0100

    Added Header and Title

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,37 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Content from '../../components/Content';
+import Title from '../../components/Title';
 
 const Dashboard = props => (
-    <div id={props.id}>
-      <button onClick={props.go} data-to="suggestion">back</button>
-      <h1>Party basket</h1>
-      <h2>Hallween Party - 17.11.2019</h2>
+    <Content id={props.id}>
+      <div style={{ paddingTop: '10vh', paddingRight: '10px', padingLeft: '10px' }}>
+        <Title>Party basket</Title>
+      
 
-      <div>
-        <p>Total sum</p>
-        <p>Amount per person</p>
-        <p>Saved money</p>
-        <p>Purchase before 13.11.2019</p>
-        <button>Purchase</button>
-      </div>
+        <button onClick={props.go} data-to="suggestion">back</button>
+        <h2>Hallween Party - 17.11.2019</h2>
 
-
-      <div>
         <div>
-          <img></img>
-          <p>Pumpkins</p>
-          <p>Stuff with pumpkins which is very cool ...</p>
-          <p>$ 4</p>
-          <div>upvote 9</div>
-          <div>downvote 1</div>
+          <p>Total sum</p>
+          <p>Amount per person</p>
+          <p>Saved money</p>
+          <p>Purchase before 13.11.2019</p>
+          <button>Purchase</button>
+        </div>
+
+
+        <div>
+          <div>
+            <img></img>
+            <p>Pumpkins</p>
+            <p>Stuff with pumpkins which is very cool ...</p>
+            <p>$ 4</p>
+            <div>upvote 9</div>
+            <div>downvote 1</div>
+          </div>
+        </div>
+
+
+        <div>
+          <p>Add</p>
         </div>
       </div>
-
-
-      <div>
-        <p>Add</p>
-      </div>
-    </div>
+    </Content>
 );
 
 Dashboard.propTypes = {
