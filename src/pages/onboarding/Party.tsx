d712a671c3f5e63d5eb1c0dbc267729111ca9d26
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-11-16 22:30:51 +0200

    Implement party and discuss

--- src/pages/onboarding/Party.tsx
+++ src/pages/onboarding/Party.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Button } from '@vkontakte/vkui';
+import party from './img/party.jpg';
 
 const Party = ({ go }: { go: (e: any) => void }) => {
   return (
-    <div style={{ marginTop: 100 }}>
+    <div style={{ textAlign: 'center' }}>
+      <img src={party} style={{ width: '100%', marginTop: '20vh' }} />
+      <h1 style={{ fontWeight: 'bold', fontSize: 24 }}>Party</h1>
+      <p style={{ fontSize: 18, padding: '0px 50px' }}>
+        Have fun on the party and don’t worry about the money.
+      </p>
       <Button size="xl" level="2" onClick={go} data-to="ideation">
         Let's go
       </Button>
