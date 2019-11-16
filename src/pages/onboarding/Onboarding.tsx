8e0897d62c0f42dc5d3c7378d21dc05b173a05cf
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-11-16 21:37:51 +0200

    add gradient

--- src/pages/onboarding/Onboarding.tsx
+++ src/pages/onboarding/Onboarding.tsx
@@ -10,6 +10,20 @@ const Onboarding = ({ id }: { id: "onboarding" }) => {
   const onChangeIndex = (newIndex: number) => setIndex(newIndex);
   return (
     <>
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: 79,
+          top: 0,
+          left: 0,
+          background:
+            "linear-gradient(180deg, #C4C4C4 0%, rgba(196, 196, 196, 0) 100%)",
+          opacity: 0.5,
+          zIndex: 100
+        }}
+      />
+
       <SwipeableViews
         style={{ height: "100vh" }}
         containerStyle={{ height: "100%" }}
